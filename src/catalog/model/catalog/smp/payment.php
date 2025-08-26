18ec249d9ef1b2a3a4d206679f4f4d1697ceddc5
Yaroslav Holovatyi <yaroslav.holovatiy@gmail.com>
2025-08-26 16:09:51 +0300

    Заготовка функцій для даних по оплатам

--- src/catalog/model/catalog/smp/payment.php
+++ src/catalog/model/catalog/smp/payment.php
@@ -8,31 +8,25 @@ class ModelCatalogSmpPayment extends Model
     public function getOrderPaymentDetails($order_id, $payment_code, $payment_method)
 	{
 		if ($payment_code == 'wayforpay') {
-			$payment_details = $this->getWayforpayPaymentData($order_id);
+			$payment_details = $this->Wayforpay($order_id);
 		} elseif ($payment_code == 'lqp') {
-			$payment_details = $this->getLiqpayPaymentData($order_id);
+			$payment_details = $this->Liqpay($order_id);
 		} else {
 			$payment_details = []; // short form for array
 		}
-		
 		return $payment_details;
-
 	}
 
-	protected function getWayforpayPaymentData(int $order_id) 
+	protected function Wayforpay(int $order_id) 
 	{
-
 		$data = [];
 		return $data;
-
 	}
 
-	protected function getLiqpayPaymentData(int $order_id)
+	protected function Liqpay(int $order_id)
 	{
-
 		$data = [];
 		return $data;
-
 	}
 
 }
